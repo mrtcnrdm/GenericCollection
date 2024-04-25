9ea763f774edb4d3e90552c5af0629315e6b3f81
Mert Can Erdem <mrtcnrdmii@gmail.com>
2024-04-25 14:00:21 +0300

    Initial commit

---
+++ GenericCollection/GenericCollection.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+
+public class GenericCollection<T> : IList<T>, ICollection<T>, IEnumerable<T>
+{
+    private List<T> items = new List<T>();
+
+    // IList<T> members
+    public T this[int index]
+    {
+        get => items[index];
+        set => items[index] = value;
+    }
+
+    public int Count => items.Count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(T item)
+    {
+        items.Add(item);
+    }
+
+    public void Clear()
+    {
+        items.Clear();
+    }
+
+    public bool Contains(T item)
+    {
+        return items.Contains(item);
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        items.CopyTo(array, arrayIndex);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return items.GetEnumerator();
+    }
+
+    public int IndexOf(T item)
+    {
+        return items.IndexOf(item);
+    }
+
+    public void Insert(int index, T item)
+    {
+        items.Insert(index, item);
+    }
+
+    public bool Remove(T item)
+    {
+        return items.Remove(item);
+    }
+
+    public void RemoveAt(int index)
+    {
+        items.RemoveAt(index);
+    }
+
+    // ICollection<T> member
+    void ICollection<T>.Add(T item)
+    {
+        Add(item);
+    }
+
+    // IEnumerable<T> member
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ GenericCollection/Program.cs
@@ -0,0 +1,43 @@
+﻿GenericCollection<int> intCollection = new GenericCollection<int>();
+
+for (int i = 0; i < 5; i++)
+{
+    intCollection.Add(i);
+}
+
+foreach (var item in intCollection)
+{
+    Console.WriteLine(item);
+}
+
+//intCollection.Remove(2);
+intCollection.RemoveAt(intCollection.Count() - 1);
+
+foreach (var item in intCollection)
+{
+    Console.WriteLine(item);
+}
+
+
+GenericCollection<string> stringCollection = new GenericCollection<string>();
+
+stringCollection.Add("Mert");
+stringCollection.Add("Can");
+stringCollection.Add("Erdem");
+
+foreach (var item in stringCollection)
+{
+    Console.WriteLine(item);
+}
+
+List<int> list = new List<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+
+
+GenericCollection<List<int>> listIntCollection = new GenericCollection<List<int>>();
+listIntCollection.Add(list);
+
+GenericCollection<IList<int>> iListIntCollection = new GenericCollection<IList<int>>();
+iListIntCollection.Add(list);
